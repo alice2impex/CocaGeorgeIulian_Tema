95766710a0000dd69dc6dcdd1dc0a08a22795d33
Coca George Iulian <Laptop@DESKTOP-SCEKIFJ>
2020-07-09 13:08:44 +0300

    Initial Commit

---
+++ Seminar1/Tests/Greeting.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Xml.Schema;
+
+namespace Seminar1.Tests
+{
+    internal class Greeting
+    {
+
+        public Greeting()
+        {
+        }
+
+        internal string Greet(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Hello, my friend.";
+            }
+
+            if (name == name.ToUpper())
+            {
+                return $"HELLO {name.ToUpper()}!";
+            }
+            if (name.Contains(","))
+            {
+                string[] words = name.Split(',');
+                int numberOfNames = words.Length;
+                int nr = 0; //Numar nume scrise cu LITERE MARI
+                string ret = "";
+                for (int i = 0; i<numberOfNames; i++)
+                {
+                    if (words[i] == words[i].ToUpper())
+                    {
+                        nr = nr + 1;
+                    }
+                }
+                if (nr == 0)
+                {
+                    if (numberOfNames == 2)
+                    {
+                        ret = "Hello, " + words[0] + " and" + words[1] + ".";
+                    }
+                    else if (numberOfNames > 2)
+                    {
+                        ret = "Hello, ";
+                        for (int i = 0; i<numberOfNames-1; i++)
+                        {
+                            ret = ret + words[i] + ", ";
+                        }
+                        ret = ret + "and " + words[numberOfNames-1] + ".";
+                    }
+                }
+                else if (nr>0)
+                {
+                    if (numberOfNames == 2)
+                    {
+                        ret = "Hello, "+ words[0] + " and "+ words[1] + ".";
+                    }
+
+                    else if (numberOfNames > 2)
+                    {
+                        ret = "Hello, ";
+                        for (int i = 0; i<numberOfNames-1; i++)
+                        {
+                            if (words[i] != words[i].ToUpper())
+                            {
+                                ret = ret + words[i] + " ";
+                            }
+                        }
+                        ret = ret + "and " + words[numberOfNames-1] + ".";
+                    }
+
+                    ret = ret + " AND HELLO ";
+                    for (int i = 0; i<numberOfNames; i++)
+                    {
+                        if (words[i] == words[i].ToUpper())
+                        {
+                            ret = ret + words[i];
+                        }
+                    }
+                    ret = ret + "!";
+                }
+                return ret;
+            }
+
+            return $"Hello, {name}.";
+        }
+    }
+}
---
+++ Seminar1/Tests/GreetingTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1.Tests
+{
+    [TestFixture]
+    public class GreetingTests
+    {
+        Greeting greeting = new Greeting();
+
+        public void ExecuteTest(string name, string expected)
+        {
+            //Arrange
+
+            //Act
+            string result = greeting.Greet(name);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ShouldGreetBob()
+        {
+            ExecuteTest("Bob", "Hello, Bob.");
+        }
+
+
+        [Test]
+        public void ShouldGreetGigi()
+        {
+            ExecuteTest("Gigi", "Hello, Gigi.");
+        }
+
+        [Test]
+        public void ShouldHandleNull()
+        {
+            ExecuteTest(null, "Hello, my friend.");
+        }
+
+        [Test]
+        public void ShouldHandleShooting()
+        {
+            ExecuteTest("JERRY", "HELLO JERRY!");
+        }
+
+        [Test]
+        public void ShouldHandleTwoNames()
+        {
+            ExecuteTest("Jill, Jane", "Hello, Jill and Jane.");
+        }
+
+        [Test]
+        public void ShouldHandleThreeNames()
+        {
+            ExecuteTest("Amy,Brian,Charlotte", "Hello, Amy, Brian, and Charlotte.");
+        }
+
+        [Test]
+        public void ShouldHandleFourNames()
+        {
+            ExecuteTest("Amy,Brian,Charlotte,Ronaldo", "Hello, Amy, Brian, Charlotte, and Ronaldo.");
+        }
+
+        [Test]
+        public void ShouldHandleMixingNames()
+        {
+            ExecuteTest("Amy,BRIAN,Charlotte", "Hello, Amy and Charlotte. AND HELLO BRIAN!");
+        }
+
+        [Test]
+        public void ShouldHandleMixingNames2()
+        {
+            ExecuteTest("AMY,Brian,Charlotte", "Hello, Brian and Charlotte. AND HELLO AMY!");
+        }
+
+
+
+    }
+}
